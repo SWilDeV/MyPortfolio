587e5f71d526eb2a38181cade33800bf62ed7baf
sean wilson <sean0669@gmail.com>
2020-08-20 16:57:11 -0400

    Work changes

--- docs/js/main.js
+++ docs/js/main.js
@@ -42,3 +42,15 @@ let showProject = false;
 function toggleProject(){
     projectItems.classList.add('show');
 };
+
+function flip(event){
+	var element = event.currentTarget;
+	if (element.className === "imgItem") {
+    if(element.style.transform == "rotateY(180deg)") {
+      element.style.transform = "rotateY(0deg)";
+    }
+    else {
+      element.style.transform = "rotateY(180deg)";
+    }
+  }
+};
